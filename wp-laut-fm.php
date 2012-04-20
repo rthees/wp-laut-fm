4e2189fd95ae20740d005302489c591cdb157391
unknown <rthees@.main-post.intern>
2012-04-20 14:53:27 +0200

    startup

---
+++ wp-laut-fm.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: laut.fm for Wordpress
+Plugin URI: http://herrthees.de/wp-laut-fm
+Description: Displays data for a webradio-station hostet on laut.fm
+Version: 0.1
+Author: Ralf Thees
+Author URI: http://herrthees.de
+License: GPL2
+*/
+
+
+?>
